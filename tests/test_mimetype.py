aaf0b4b8381b60a60f94e5a657a96aa28df64c81
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2020-10-24 23:24:26 -0400

    add mimetype tests

---
+++ tests/test_mimetype.py
@@ -0,0 +1,15 @@
+import unittest
+
+import mimetype
+
+class MimetypeTest(unittest.TestCase):
+	def test_guess_mimetype_extension_dict(self):
+		self.assertEqual(mimetype.guessExtension("image/jpeg"), "jpg")
+
+
+	def test_guess_mimetype_extension_guess(self):
+		self.assertEqual(mimetype.guessExtension("application/zip"), "zip")
+
+
+	def test_guess_mimetype_extension_fail(self):
+		self.assertIsNone(mimetype.guessExtension("???"))
